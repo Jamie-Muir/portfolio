2052fdb8448666fdc1c356e2eeb462579dfeb209
jamie-muir <jamiemuircs@gmail.com>
2022-11-28 08:14:59 +0000

    img

--- src/components/blocks/Projects.js
+++ src/components/blocks/Projects.js
@@ -30,17 +30,22 @@ function Projects() {
 				subtitle='Website Redesign'
 				className='gridItem'
 				background={gramp}
-				live='https://jamie-muir.github.io/grampian-secure/'
+				// live='https://jamie-muir.github.io/grampian-secure/'
 				//'https://www.grampiansecure.co.uk'
 				github='https://github.com/Jamie-Muir/grampian-secure'
 				html css js react
 			>
-				A website redesign project for a local security company with the purpose of building a faster and smoother user experience than their current solution that could also be decoupled from their hosting service.
+				A website redesign project for a local security company with the purpose of building a faster and smoother user
+				experience than their current solution that could also be decoupled from their hosting service.
 				<br /><br />
 				The initial site had a speed index of up to 10.6s at worst, according to lightbox.
-				My redesign dramatically improved the fluidity and speed of the site. I accomplished this by building it as a Single-Page Application with React, and utilising more advanced features such as lazy-loading to improve the initial load time.
+
+				My redesign dramatically improved the fluidity and speed of the site. I accomplished this by building it as a
+				Single-Page Application with React, and utilising more advanced features such as lazy-loading to improve the
+				initial load time.
 				<br /><br />
-				Along with other improvements like converting all images to the Webp format the resulting design had a reported speed index of under 1 second.
+				Along with other improvements like converting all images to the Webp format the resulting design had a reported
+				speed index of under 1 second.
 
 			</ProjectCard>
 
@@ -54,13 +59,21 @@ function Projects() {
 				github='https://github.com/Jamie-Muir/LobsangBot'
 				js
 			>
-				Lobsang Bot is an application built in Javascript utilising the Discord API to provide moderation capabilities via text-line to Discord servers.
+				Lobsang Bot is an application built in Javascript utilising the Discord API to provide moderation capabilities
+				via text-line to Discord servers.
 				<br />
-				This project was developed as an exploration of text interfaces in javascript and provided a practice ground to utilise ES6 features and module exports in creating a highly scalable project.
+				This project was developed as an exploration of text interfaces in javascript and provided a practice ground to
+				utilise ES6 features and module exports in creating a highly scalable project.
 				<br /><br />
-				Exporting commands/functions as an object that contained meta data about the function was one of my favourite structural elements of this project and a great learning experience in moving data around in Javascript. How well the functions are related to the user and maintainer is something I consider a success of the project and was a good precursor to developing my skills in React.
+				Exporting commands/functions as an object that contained meta data about the function was one of my favourite
+				structural elements of this project and a great learning experience in moving data around in Javascript. How well
+				the functions are related to the user and maintainer is something I consider a success of the project and was a
+				good precursor to developing my skills in React.
+
 				<br /><br />
-				If I were to revisit this project, I would take what I've learned from React in compartmentalising file systems and focus on building a better overall file structure for the project as I feel that, along with function bloat in certain places, are the core areas to learn from with this project.
+				If I were to revisit this project, I would take what I've learned from React in compartmentalising file systems
+				and focus on building a better overall file structure for the project as I feel that, along with function bloat
+				in certain places, are the core areas to learn from with this project.
 
 			</ProjectCard>
 
@@ -71,9 +84,14 @@ function Projects() {
 				subtitle='Practice Applet'
 				className='gridItem'
 				background={expenses}
+				live='https://jamie-muir.github.io/react-expenses/'
+				github='https://github.com/Jamie-Muir/react-expenses'
 				html css js react
 			>
-				This is a card I think
+				A twist on the typical To-Do C.R.U.D practice app.
+				<br /> <br />
+				Base UI is from Academind's React - The Complete Guide course, I have extended the functionality to include a
+				Firebase backend.
 			</ProjectCard>
 
 			{/* NUMBER 4 */}
